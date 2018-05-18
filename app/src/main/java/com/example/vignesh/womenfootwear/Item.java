82abfce9bfd6e8823d4f579389e753dbf01f9066
Vignesh <vigneshit13@gmail.com>
2018-05-18 20:09:00 +0530

    Updated

--- app/src/main/java/com/example/vignesh/womenfootwear/Item.java
+++ app/src/main/java/com/example/vignesh/womenfootwear/Item.java
@@ -10,16 +10,18 @@ import android.widget.Toast;
  * Created by vignesh on 18/5/18.
  */
 
-public class Item extends MainActivity {
+public class Item extends MainActivity implements RecyclerViewAdapter.ItemListener{
     TextView tv1, tv2, tv3;
     String a, b, c;
     Button btn;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.item);
+
         tv1 = (TextView) findViewById(R.id.textView7);
         tv2 = (TextView) findViewById(R.id.textView8);
         tv3 = (TextView) findViewById(R.id.textView9);
+
         tv1.setText(a);
         tv2.setText(b);
         tv3.setText(c);
@@ -27,8 +29,9 @@ public class Item extends MainActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(Item.this,a+" "+b+" "+c,Toast.LENGTH_SHORT).show();
+                Toast.makeText(Item.this,"Added Succesfully",Toast.LENGTH_SHORT).show();
             }
         });
+
     }
 }
--- app/src/main/java/com/example/vignesh/womenfootwear/refine.java
+++ app/src/main/java/com/example/vignesh/womenfootwear/refine.java
@@ -121,4 +121,3 @@ public class refine extends MainActivity {
     }
 }
 
-    
